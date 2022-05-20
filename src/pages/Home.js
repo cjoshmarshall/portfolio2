847c00bfe476b21a96938491b386ad18c2acbffa
unknown <cjoshmarshall@gmail.com>
2022-05-20 21:41:16 +0530

    fix 1.5

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,14 +5,13 @@ function Home() {
     return (
       <div className="home">
         <div className='home_container'>
-          <h1 className='home_title'>Joshua Marshall</h1>
-          <p className='home_description'><span>FULL STACK DEVELOPER</span></p>
+          <h1 className='home_description'>Hi, I'm <span>Josh</span> and I build swift, smooth <span>&</span> stunning websites. </h1>
           <div className='home_buttonContainer'>
             <a href='https://drive.google.com/file/d/1VaOT_bj3izsFctooxw18qdH_QrFhDkNu/view' target="_blank" rel="noopener noreferrer">
-              <button className='home_button'>VIEW RESUME</button>
+              <button className='home_button'>CHECK OUT MY RESUME</button>
             </a>
             <a href='https://www.github.com/cjoshmarshall' target="_blank" rel="noopener noreferrer">
-              <button className='home_button'>VIEW GITHUB</button>
+              <button className='home_button'>GO TO MY GITHUB PROFILE</button>
             </a>
           </div>
         </div>
