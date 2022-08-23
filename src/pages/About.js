d711f9590da3d64f20f6e44d20e842c230171ab2
josh <cjoshmarshall@gmail.com>
2022-08-23 23:57:03 +0530

    Modified Facts

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@ function About() {
         <h1 className='about_title'>About me</h1>
         <div className='about_subcontainer'>
           <i className='about_description'>Hi, I am Joshua Marshall. I'm a <span>Full Stack Developer</span> focusing on the <span>MERN</span> stack, currently based in Tirunelveli, Tamilnadu.</i>
-          <i className='about_description'>I got a bachelor's degree on Electronics and Communications Engineering with a GPA of 6.62</i>
+          <i className='about_description'>I got a bachelor's degree on Electronics and Communications Engineering.</i>
         </div>
         <div className='about_subcontainer'>
           <i className='about_description'>I mostly specialize in Front-End, whether pinching creative ideas, even designing and building the whole website from scratch with the Back-End.</i>
