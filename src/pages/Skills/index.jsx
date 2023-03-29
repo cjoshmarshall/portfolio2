4d980ed2dd1df6b566228aeefeb9f2cd50e15aa8
Josh Marshall <cjoshmarshall@gmail.com>
2023-03-29 17:51:40 +0530

    Skills Update

--- src/pages/Skills/index.jsx
+++ src/pages/Skills/index.jsx
@@ -7,61 +7,65 @@ function Skills() {
       <div className="skills_container">
         <h1 className="skills_title">Skills</h1>
         <div className="skills_subcontainer">
-          <div className="skills_innercontainer">
+          <div className="skills_innercontainer skills_html">
             <i className="skills_icon fa-brands fa-html5"></i>
             <p>HTML</p>
           </div>
-          <div className="skills_innercontainer">
+          <div className="skills_innercontainer skills_css">
             <i className="skills_icon fa-brands fa-css3-alt"></i>
             <p>CSS</p>
           </div>
-          <div className="skills_innercontainer">
+          <div className="skills_innercontainer skills_js">
             <i className="skills_icon fa-brands fa-js-square"></i>
             <p>Javascript</p>
           </div>
-          <div className="skills_innercontainer">
+          <div className="skills_innercontainer skills_ts">
             <i class="skills_icon fa-solid fa-scroll"></i>
             <p>Typescript</p>
           </div>
-          <div className="skills_innercontainer">
+          <div className="skills_innercontainer skills_bootstrap">
             <i className="skills_icon fa-brands fa-bootstrap"></i>
             <p>Bootstrap</p>
           </div>
-          <div className="skills_innercontainer">
+          <div className="skills_innercontainer skills_react">
             <i className="skills_icon fa-brands fa-react"></i>
-            <p>ReactJS</p>
+            <p>React</p>
           </div>
-          <div className="skills_innercontainer">
+          <div className="skills_innercontainer skills_next">
+            <i className="skills_icon fa-solid fa-n"></i>
+            <p>Next.js</p>
+          </div>
+          <div className="skills_innercontainer skills_native">
+            <i className="skills_icon fa-brands fa-react"></i>
+            <p>React Native</p>
+          </div>
+          <div className="skills_innercontainer skills_node">
             <i className="skills_icon fa-brands fa-node-js"></i>
-            <p>NodeJS</p>
+            <p>Node.js</p>
           </div>
-          <div className="skills_innercontainer">
+          <div className="skills_innercontainer skills_mysql">
             <i className="skills_icon fa-solid fa-database"></i>
             <p>MySQL</p>
           </div>
-          <div className="skills_innercontainer">
+          <div className="skills_innercontainer skills_mongo">
             <i className="skills_icon fa-solid fa-leaf"></i>
             <p>MongoDB</p>
           </div>
-          <div className="skills_innercontainer">
+          <div className="skills_innercontainer skills_git">
             <i className="skills_icon fa-brands fa-git-alt"></i>
             <p>Git</p>
           </div>
-          <div className="skills_innercontainer">
+          <div className="skills_innercontainer skills_npm">
             <i className="skills_icon fa-brands fa-npm"></i>
             <p>npm</p>
           </div>
-          <div className="skills_innercontainer">
+          <div className="skills_innercontainer skills_yarn">
             <i className="skills_icon fa-brands fa-yarn"></i>
             <p>yarn</p>
           </div>
-          <div className="skills_innercontainer">
+          <div className="skills_innercontainer skills_aws">
             <i className="skills_icon fa-solid fa-cloud"></i>
             <p>AWS</p>
-          </div>
-          <div className="skills_innercontainer">
-            <i className="skills_icon fa-solid fa-eye"></i>
-            <p>Photoshop</p>
           </div>
         </div>
       </div>
